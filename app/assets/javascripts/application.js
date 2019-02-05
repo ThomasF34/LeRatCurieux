90c3f0645b449fb89df5bae6638a42c3a0daacfa
Thomas Falcone <thomas.falcone06@gmail.com>
2019-02-05 19:34:40 +0100

    Add(Slider without correct images & names)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,34 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require jquery3
 //= require popper
 //= require bootstrap
 //= require_tree .
+//= require owl.carousel
+
+$(window).on('load', function(){
+	$('.owl-carousel').owlCarousel({
+		margin:10,
+		loop:true,
+		autoplay:true,
+		autoplayTimeout:2000,
+		autoplayHoverPause:true,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:true
+			},
+			600:{
+				items:3,
+				nav:false
+			},
+			1000:{
+				items:5,
+				nav:true,
+				loop:false
+			}
+		}
+	})
+})
