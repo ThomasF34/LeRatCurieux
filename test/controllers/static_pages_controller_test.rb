092b1c24ca5b149c64d8b12e82f35ff5c247e06f
Thomas Falcone <thomas.falcone06@gmail.com>
2019-02-27 20:55:17 +0100

    Fix(Clean code) Add(Tests)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Le Rat Curieux"
+  end
+  
+  test "should get root" do 
+    get root_url
+    assert_response :success
+  end 
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "#{@base_title}"
+
   end
 
+  test "should get privacy" do 
+    get privacy_url
+    assert_response :success
+    assert_select "title", "Confidentialité - #{@base_title}"
+  end 
+
+  test "shoud get contact" do 
+    get contact_url
+    assert_response :success
+    assert_select "title", "Contact - #{@base_title}"
+  end
 end
